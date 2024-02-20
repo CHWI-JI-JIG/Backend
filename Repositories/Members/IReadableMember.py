df4b9b9dc90e33d20c20e168c07db5d7055fd762
taktakgyun <vbmrkk@naver.com>
2024-02-20 20:28:34 +0900

    ✨feat : Add ReadableMember

---
+++ Repositories/Members/IReadableMember.py
@@ -0,0 +1,22 @@
+import __init__
+from abc import ABCMeta, abstractmethod
+from typing import Optional, Tuple
+from result import Result
+
+from Domains.Members import *
+
+
+class IReadableMember(metaclass=ABCMeta):
+    @abstractmethod
+    def check_exist_account(self, account: str) -> bool: ...
+
+    @abstractmethod
+    def get_member(self, member_id: MemberID) -> Result[Member, str]: ...
+
+    @abstractmethod
+    def get_privacy(self, member_id: MemberID) -> Result[Privacy, str]: ...
+
+    @abstractmethod
+    def get_member_and_privacy(
+        self, member_id: MemberID
+    ) -> Result[Tuple[Member, Privacy], str]: ...
