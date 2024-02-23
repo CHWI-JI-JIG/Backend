c882fe51edd1b9b36f0ec8b03fcef894d90d94b2
taktakgyun <vbmrkk@naver.com>
2024-02-23 18:26:41 +0900

    ✅ test: code

--- Tests/Members/test_member_service.py
+++ Tests/Members/test_member_service.py
@@ -4,13 +4,13 @@ import sys
 
 
 from Builders.Members import *
-from Storages.Members import MySqlSaveMember,LoginVerifiableAuthentication
+from Storages.Members import MySqlSaveMember, LoginVerifiableAuthentication
 from Domains.Members import *
 
 from Migrations import MySqlCreateUser, MySqlCreateProduct
 from Applications.Members import CreateMemberService
 from Applications.Members.ExtentionMethod import hashing_passwd
-from result import Result,Ok, Err, is_ok
+from result import Result, Ok, Err, is_ok
 
 from icecream import ic
 
@@ -25,7 +25,7 @@ class test_member_service(unittest.TestCase):
         # product_migrate = MySqlCreateProduct(test_padding)
         cls.mysql_save_member = MySqlSaveMember(test_padding)
         cls.create_service = CreateMemberService(cls.mysql_save_member)
-        cls.l_repo=LoginVerifiableAuthentication(test_padding)
+        cls.l_repo = LoginVerifiableAuthentication(test_padding)
         if cls.user_migrate.check_exist_user():
             cls.user_migrate.delete_user()
 
@@ -77,14 +77,11 @@ class test_member_service(unittest.TestCase):
         print("\t", sys._getframe(0).f_code.co_name)
         if self.user_migrate.check_exist_user():
             self.user_migrate.delete_user()
-    
-    
-    
-    
+
     def test_정상_회원(self):
         "Hook method for deconstructing the test fixture after testing it."
         print("\t\t", sys._getframe(0).f_code.co_name)
-    
+
         # Create seller
         uid = self.create_service.create(
             account="seller1",
@@ -98,15 +95,16 @@ class test_member_service(unittest.TestCase):
             pay_account="11255023855646523",
         )
 
-        self.assertIsInstance(uid,Ok)
-        login_result = self.l_repo.identify_and_authenticate('seller1',hashing_passwd('123456'))
+        self.assertIsInstance(uid, Ok)
+        login_result = self.l_repo.identify_and_authenticate(
+            "seller1", hashing_passwd("123456")
+        )
         match login_result:
             case Ok(auth):
                 self.assertTrue(auth.is_sucess)
             case Err:
                 raise ValueError()
-            
-            
+
         # Create buyer
         uid = self.create_service.create(
             account="buyer1",
@@ -118,21 +116,20 @@ class test_member_service(unittest.TestCase):
             address="서울시 서초구",
         )
 
-        self.assertIsInstance(uid,Ok)
-        login_result = self.l_repo.identify_and_authenticate('buyer1',hashing_passwd('12345'))
+        self.assertIsInstance(uid, Ok)
+        login_result = self.l_repo.identify_and_authenticate(
+            "buyer1", hashing_passwd("12345")
+        )
         match login_result:
             case Ok(auth):
                 self.assertTrue(auth.is_sucess)
             case Err:
                 raise ValueError()
-        
-          
-
 
     def test_비정상_회원(self):
         "Hook method for deconstructing the test fixture after testing it."
         print("\t\t", sys._getframe(0).f_code.co_name)
-        
+
         # Create seller
         uid = self.create_service.create(
             account="1q2w2e34r",
@@ -145,9 +142,8 @@ class test_member_service(unittest.TestCase):
             company_registration_number="1466555462",
             pay_account="11255023855444523",
         )
-        self.assertIsInstance(uid,Err)
-            
-            
+        self.assertIsInstance(uid, Err)
+
         # Create buyer
         uid = self.create_service.create(
             account="zxcvbn",
@@ -158,26 +154,20 @@ class test_member_service(unittest.TestCase):
             email="buyerhi@naver.com",
             address="서울시 동작구",
         )
-        self.assertIsInstance(uid,Err)
+        self.assertIsInstance(uid, Err)
 
-            
-            
-        
     def test_로그인_성공(self):
         "Hook method for deconstructing the test fixture after testing it."
         print("\t\t", sys._getframe(0).f_code.co_name)
-        
-        
-        
-        
-        
 
     def test_로그인_실패(self):
         "Hook method for deconstructing the test fixture after testing it."
         print("\t\t", sys._getframe(0).f_code.co_name)
-        
+
         # seller (비밀번호 불일치 = 실패)
-        login_result = self.l_repo.identify_and_authenticate('qazwsx',hashing_passwd('123--'))
+        login_result = self.l_repo.identify_and_authenticate(
+            "qazwsx", hashing_passwd("123--")
+        )
         match login_result:
             case Ok(auth):
                 self.assertEqual(auth.fail_count, 0)
@@ -186,8 +176,10 @@ class test_member_service(unittest.TestCase):
             case Err:
                 self.assertFalse(True)
 
-        # (비밀번호 불일치 = 실패)       
-        login_result = self.l_repo.identify_and_authenticate('qazwsx',hashing_passwd('123--'))
+        # (비밀번호 불일치 = 실패)
+        login_result = self.l_repo.identify_and_authenticate(
+            "qazwsx", hashing_passwd("123--")
+        )
         match login_result:
             case Ok(auth):
                 self.assertEqual(auth.fail_count, 1)  # 위에서 실패해서 +1
@@ -195,9 +187,11 @@ class test_member_service(unittest.TestCase):
                 self.assertIsNone(ret.ok())
             case Err:
                 self.assertFalse(True)
-        
+
         # (비밀번호 일치 = 성공)
-        login_result = self.l_repo.identify_and_authenticate('qazwsx',hashing_passwd('123'))
+        login_result = self.l_repo.identify_and_authenticate(
+            "qazwsx", hashing_passwd("123")
+        )
         match login_result:
             case Ok(auth):
                 self.assertEqual(auth.fail_count, 2)  # 위에서 실패해서 +1
@@ -205,9 +199,11 @@ class test_member_service(unittest.TestCase):
                 self.assertIsNone(ret.ok())
             case Err:
                 self.assertFalse(True)
-        
-        # (비밀번호 불일치 = 실패)       
-        login_result = self.l_repo.identify_and_authenticate('qazwsx',hashing_passwd('123--'))
+
+        # (비밀번호 불일치 = 실패)
+        login_result = self.l_repo.identify_and_authenticate(
+            "qazwsx", hashing_passwd("123--")
+        )
         match login_result:
             case Ok(auth):
                 self.assertEqual(auth.fail_count, 0)  # 위에서 성공해서 0
@@ -216,10 +212,10 @@ class test_member_service(unittest.TestCase):
             case Err:
                 self.assertFalse(True)
 
-                
-                
         # buyer (비밀번호 불일치 = 실패)
-        login_result = self.l_repo.identify_and_authenticate('zxcvbn',hashing_passwd('123--'))
+        login_result = self.l_repo.identify_and_authenticate(
+            "zxcvbn", hashing_passwd("123--")
+        )
         match login_result:
             case Ok(auth):
                 self.assertEqual(auth.fail_count, 0)
@@ -227,40 +223,42 @@ class test_member_service(unittest.TestCase):
                 self.assertIsNone(ret.ok())
             case Err:
                 self.assertFalse(True)
-                
-        
-        # (비밀번호 불일치 = 실패)         
-        login_result = self.l_repo.identify_and_authenticate('zxcvbn',hashing_passwd('123--'))
-        match login_result:
-            case Ok(auth):
-                self.assertEqual(auth.fail_count, 1) # 위에서 실패해서 +1
-                ret = self.l_repo.update_access(auth)
-                self.assertIsNone(ret.ok())
-            case Err:
-                self.assertFalse(True)
-                
-        # (비밀번호 일치 = 성공)        
-        login_result = self.l_repo.identify_and_authenticate('zxcvbn',hashing_passwd('123'))
-        match login_result:
-            case Ok(auth):
-                self.assertEqual(auth.fail_count, 2) # 위에서 실패해서 +1
-                ret = self.l_repo.update_access(auth)
-                self.assertIsNone(ret.ok())
-            case Err:
-                self.assertFalse(True)
-                
-        
-        # (비밀번호 불일치 = 실패)          
-        login_result = self.l_repo.identify_and_authenticate('zxcvbn',hashing_passwd('123--'))
-        match login_result:
-            case Ok(auth):
-                self.assertEqual(auth.fail_count, 0) # 위에서 성공해서 0
-                ret = self.l_repo.update_access(auth)
-                self.assertIsNone(ret.ok())
-            case Err:
-                self.assertFalse(True)
-            
 
+        # (비밀번호 불일치 = 실패)
+        login_result = self.l_repo.identify_and_authenticate(
+            "zxcvbn", hashing_passwd("123--")
+        )
+        match login_result:
+            case Ok(auth):
+                self.assertEqual(auth.fail_count, 1)  # 위에서 실패해서 +1
+                ret = self.l_repo.update_access(auth)
+                self.assertIsNone(ret.ok())
+            case Err:
+                self.assertFalse(True)
+
+        # (비밀번호 일치 = 성공)
+        login_result = self.l_repo.identify_and_authenticate(
+            "zxcvbn", hashing_passwd("123")
+        )
+        match login_result:
+            case Ok(auth):
+                self.assertEqual(auth.fail_count, 2)  # 위에서 실패해서 +1
+                ret = self.l_repo.update_access(auth)
+                self.assertIsNone(ret.ok())
+            case Err:
+                self.assertFalse(True)
+
+        # (비밀번호 불일치 = 실패)
+        login_result = self.l_repo.identify_and_authenticate(
+            "zxcvbn", hashing_passwd("123--")
+        )
+        match login_result:
+            case Ok(auth):
+                self.assertEqual(auth.fail_count, 0)  # 위에서 성공해서 0
+                ret = self.l_repo.update_access(auth)
+                self.assertIsNone(ret.ok())
+            case Err:
+                self.assertFalse(True)
 
 
 def main():
