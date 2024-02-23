aa7df125cab1a2be5e8f8bb7a5e63d8eec24afa4
Ryu9999 <snddjk234@naver.com>
2024-02-23 19:16:47 +0900

    ⚡️ Django API signup

--- Django/Django/urls.py
+++ Django/Django/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from Members.view import member_view
+from Members.view import signup_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/', member_view.signup, name='signup'),
+    path('signup/', signup_view.signup, name='signup'),
+    path('b-sign-up/', signup_view.signup, name='signup'),
 ]
