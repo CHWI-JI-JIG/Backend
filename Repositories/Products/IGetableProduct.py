da9e215b457ea6a260a83d4ef089bd2d6175afe0
taktakgyun <vbmrkk@naver.com>
2024-02-25 16:09:46 +0900

    🗃️db : Add get product repo

--- Repositories/Products/IGetableProduct.py
+++ Repositories/Products/IGetableProduct.py
@@ -1,6 +1,6 @@
 import __init__
 from abc import ABCMeta, abstractmethod
-from typing import Optional, List
+from typing import Optional, List, Tuple
 from result import Result
 from uuid import UUID
 
@@ -10,8 +10,54 @@ from Domains.Products import *
 
 class IGetableProduct(metaclass=ABCMeta):
     @abstractmethod
-    def get_product_by_create_date(
+    def get_product_by_product_id(self, product_id: ProductID) -> Optional[Product]: ...
+
+    @abstractmethod
+    def get_products_by_create_date(
         self,
         page=0,
         size=10,
-    ) -> Result[List[Product], str]: ...
+    ) -> Result[Tuple[int, List[Product]], str]:
+        """_summary_
+
+        Args:
+            page (int, optional): _description_. Defaults to 0.
+            size (int, optional): _description_. Defaults to 10.
+
+        Returns:
+            Result[Tuple[int,List[Product]], str]:
+                Ok( int, list ): int=> count of list max, list=> result
+                Err(str): reason of Fail
+        """
+
+    @abstractmethod
+    def get_products_by_seller_id(
+        self,
+        seller_id: MemberID,
+        page=0,
+        size=10,
+    ) -> Result[Tuple[int, List[Product]], str]:
+        """_summary_
+        Product to look up Products with the same seller_id.
+
+        Returns:
+            Result[Tuple[int,List[Product]], str]:
+                Ok( int, list ): int=> count of list max, list=> result
+                Err(str): reason of Fail
+        """
+
+    @abstractmethod
+    def get_products_by_buyer_id_order(
+        self,
+        buyer_id: MemberID,
+        page=0,
+        size=10,
+    ) -> Result[Tuple[int, List[Product]], str]:
+        """_summary_
+        Get Products with the same buyer_id from an order.
+
+        Returns:
+            Result[Tuple[int,List[Product]], str]:
+                Ok( int, list ): int=> count of list max, list=> result
+                Err(str): reason of Fail
+        """
