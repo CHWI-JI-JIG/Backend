3c4f873188d6c18dc433cad7f59da81bdfb71011
taktakgyun <vbmrkk@naver.com>
2024-02-25 17:11:34 +0900

    ✨ feat : Add Domain/Comments

---
+++ Domains/Comments/__init__.py
@@ -0,0 +1,24 @@
+import sys
+from pathlib import Path
+
+now_path = Path(__file__).resolve().parent
+root_path = now_path.parent
+
+if not (str(root_path) in sys.path):
+    sys.path.append(str(root_path))
+
+from Domains.Comments.ProductID import (
+    CommentID,
+    ICommentIDBuilder,
+)
+from Domains.Comments.Comment import (
+    Comment,
+    ICommentBuilder,
+)
+
+__all__ = [
+    "CommentID",
+    "ICommentIDBuilder",
+    "Comment",
+    "ICommentBuilder",
+]
