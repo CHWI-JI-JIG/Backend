6e32854785f648563a0f65615f5470e36011ed39
taktakgyun <vbmrkk@naver.com>
2024-02-26 20:20:42 +0900

    🐛 🐛 🐛 🐛 🐛 🐛 🐛 🐛 🐛 🐛 🐛

--- Storages/Members/__init__.py
+++ Storages/Members/__init__.py
@@ -2,7 +2,7 @@ import sys
 from pathlib import Path
 
 now_path = Path(__file__).resolve().parent
-root_path = now_path.parent.parents
+root_path = now_path.parent.parent
 
 if not (str(root_path) in sys.path):
     sys.path.append(str(root_path))
@@ -10,7 +10,7 @@ if not (str(root_path) in sys.path):
 from Storages.Members.MySqlSaveMember import MySqlSaveMember
 from Storages.Members.LoginVerifiableAuthentication import LoginVerifiableAuthentication
 
-__all__=[
+__all__ = [
     "MySqlSaveMember",
     "LoginVerifiableAuthentication",
 ]
--- Storages/Sessions/__init__.py
+++ Storages/Sessions/__init__.py
@@ -2,13 +2,13 @@ import sys
 from pathlib import Path
 
 now_path = Path(__file__).resolve().parent
-root_path = now_path.parent.parents
+root_path = now_path.parent.parent
 
 if not (str(root_path) in sys.path):
     sys.path.append(str(root_path))
 
 from Storages.Sessions.MakeSaveMemberSession import MakeSaveMemberSession
 
-__all__=[
+__all__ = [
     "MakeSaveMemberSession",
 ]
