a413b07af62c6d9bd70b20f0789f60c15a74cb36
kjh1232 <kjihee0224@naver.com>
2024-02-26 19:05:40 +0900

    🐛 2_26_testproduct

--- Storages/Members/__init__.py
+++ Storages/Members/__init__.py
@@ -9,3 +9,8 @@ if not (str(root_path) in sys.path):
 
 from Storages.Members.MySqlSaveMember import MySqlSaveMember
 from Storages.Members.LoginVerifiableAuthentication import LoginVerifiableAuthentication
+
+__all__=[
+    "MySqlSaveMember",
+    "LoginVerifiableAuthentication",
+]
