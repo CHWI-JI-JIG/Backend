308b6302a086f82cb1ca5b5a61414e8e2f24ba31
taktakgyun <vbmrkk@naver.com>
2024-02-27 10:29:49 +0900

    🐛 : init dug

--- Domains/Sessions/__init__.py
+++ Domains/Sessions/__init__.py
@@ -2,13 +2,14 @@ import sys
 from pathlib import Path
 
 now_path = Path(__file__).resolve().parent
-root_path = now_path.parent.parents
+root_path = now_path.parent.parent
 
 if not (str(root_path) in sys.path):
     sys.path.append(str(root_path))
 
 from Domains.Sessions.ISession import ISesseionBuilder, ISessionSerializeable
 from Domains.Sessions.MemberSession import MemberSession, MemberSessionBuilder
+from Domains.Pro
 
 __all__ = [
     "ISessionSerializeable",
