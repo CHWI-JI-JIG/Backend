af2e9b0c8e3b930fd8fd282a65a3fc2323014f6f
ho <lsjkeh9683@naver.com>
2024-02-27 12:53:19 +0900

    ho2

--- flask/main.py
+++ flask/main.py
@@ -1,7 +1,7 @@
 import __init__
 
 from flask import Flask, session, jsonify, request
-#from Applications.Members.CreateMemberService import CreateMemberService
+from Applications.Members.CreateMemberService import CreateMemberService
 from Applications.Members.LoginMemberService import AuthenticationMemberService
 from get_config_data import get_db_padding
 from icecream import ic
@@ -15,10 +15,13 @@ from Domains.Sessions import MemberSession
 import sys
 from pathlib import Path
 
+SECRETSPATH = __init__.root_path/"secrets.json"
 
+with SECRETSPATH.open('r') as f:
+    secrets = jsonify.load(f)
 
 app = Flask(__name__)
-app.secret_key = "zxcvqwer"
+app.secret_key = secrets['SECRET_KEY']
 
 @app.route('/api/login', methods=['POST'])
 def login():
@@ -39,10 +42,56 @@ def login():
             ic(member_session)
             session['key'] = member_session.get_id()
             session['auth'] = member_session.role.name
-            return jsonify({'success': 'true', 'certification' : 'true','key': member_session.get_id(), 'auth' : str(member_session.role.name), 'name': str(member_session.name)}), 200
+            return jsonify({'success': True, 'certification' : 'true','key': member_session.get_id(), 'auth' : str(member_session.role.name), 'name': str(member_session.name)}), 200
         case Err(e):
-            return jsonify({'success': 'false'}), 401
+            return jsonify({'success': False}), 401
 
+@app.route('/api/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    
+    account = data.get('buyerId')
+    passwd = data.get('buyerPassword')
+    role = "buyer"
+    name = data.get('buyerName')
+    phone = data.get('buyerPhone')
+    email = data.get('buyerEmail')
+    address = data.get('buyerAddress')
+    
+    
+    save_member_repo = MySqlSaveMember(get_db_padding())
+    member_service = CreateMemberService(save_member_repo)
+    
+    result = member_service.create(account, passwd, role, name, phone, email, address)
+    
+    if result.is_ok():
+        return jsonify({'success': True}), 200
+    else:
+        return jsonify({'success': False})
+    
+@app.route('/api/b-signup', methods=['POST'])
+def bsignup():
+    data = request.get_json()
+    
+    account = data.get('sellerId')
+    passwd = data.get('sellerPassword')
+    role = "seller"
+    name = data.get('sellerName')
+    phone = data.get('sellerPhone')
+    email = data.get('sellerEmail')
+    address = data.get('sellerAddress')
+    company_registration_number = data.get('sellerBRN')
+    pay_account = data.get('sellerBankAccount')
+    
+    save_member_repo = MySqlSaveMember(get_db_padding())
+    member_service = CreateMemberService(save_member_repo)
+    
+    result = member_service.create(account, passwd, role, name, phone, email, address, company_registration_number, pay_account)
+    
+    if result.is_ok():
+        return jsonify({'success': True}), 200
+    else:
+        return jsonify({'success': False})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
