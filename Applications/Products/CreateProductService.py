b1049a91c2f110cdd790cb34741ca5389b182d4c
taktakgyun <vbmrkk@naver.com>
2024-02-27 12:54:53 +0900

    ✨ feat : Add Product Session

---
+++ Applications/Products/CreateProductService.py
@@ -0,0 +1,107 @@
+import __init__
+from abc import ABCMeta, abstractmethod
+from typing import Optional
+from result import Result, Ok, Err
+
+from Domains.Members import *
+from Builders.Members import *
+from Repositories.Members import *
+from Applications.Members.ExtentionMethod import hashing_passwd
+
+from icecream import ic
+
+
+class CreateProductService:
+    def __init__(
+        self,
+        # read_member_repo: IReadableMember,
+        save_member_repo: ISaveableMember,
+    ):
+        # self.read_repo = read_member_repo
+        assert issubclass(
+            type(save_member_repo), ISaveableMember
+        ), "save_member_repo must be a class that inherits from ISaveableMember."
+
+        self.save_repo = save_member_repo
+    def publish_temp_product_id(self, member_session_key:str)->
+
+    def create(
+        self,
+        account: str,
+        passwd: str,
+        role: str,
+        name: str,
+        phone: str,
+        email: str,
+        address: str,
+        company_registration_number: Optional[str] = None,
+        pay_account: Optional[str] = None,
+    ) -> Result[MemberID, str]:
+        """_summary_
+
+        Assert:
+
+
+        Returns:
+            Optional[Err]:
+                Ok :Sucess
+                Err : Reason for failure
+                    "AccountAlreadyExists: Fail_CreateMemberService_AccountAlreadyExists"
+                    "NotSameRole: There is no such thing as a {role} role."
+                    "NoHaveRegistration: If User is Seller, Then Paramater need Company registration number."
+                    "HavePayAccount: If User is Buyer, Then Paramater don't need Pay Account."
+                    "HavePayAccount: If User is Buyer, Then Paramater don't need Pay Account."
+
+        """
+
+        member_builder = NoFilterMemberBuilder(passwd_converter=hashing_passwd)
+        privacy_builder = NoFilterPrivacyBuilder(
+            name=name,
+            phone=phone,
+            email=email,
+            address=address,
+        )
+        member_builder.set_account(account=account).set_passwd(passwd)
+
+        match role:
+            case "seller":
+                if not isinstance(company_registration_number, str):
+                    return Err(
+                        "NoHaveRegistration: If User is Seller, Then Paramater need Company registration number."
+                    )
+                if not isinstance(pay_account, str):
+                    return Err(
+                        "NoHavePayAccount: If User is Seller, Then Paramater need Pay Account."
+                    )
+                member_builder.set_role(role)
+                privacy_builder.set_pay_account(
+                    pay_account
+                ).set_company_registration_number(company_registration_number)
+            case "buyer":
+                if company_registration_number is not None:
+                    return Err(
+                        "HaveRegistration: If User is Buyer, Then Paramater don't need Company registration number."
+                    )
+                if pay_account is not None:
+                    return Err(
+                        "HavePayAccount: If User is Buyer, Then Paramater don't need Pay Account."
+                    )
+                member_builder.set_role(role)
+            case "admin":
+                assert False, "NotImplementError: admin"
+                if company_registration_number is not None:
+                    return Err(
+                        "NoHaveRegistration: If User is Seller, Then Paramater need Company registration number."
+                    )
+                if pay_account is not None:
+                    return Err(
+                        "NoHavePayAccount: If User is Seller, Then Paramater need Pay Account."
+                    )
+                member_builder.set_role(role)
+            case _:
+                return Err(f"NotSameRole: There is no such thing as a {role} role.")
+        id = MemberIDBuilder().set_uuid().build()
+        return self.save_repo.save_member(
+            member=member_builder.set_id(id).build(),
+            privacy=privacy_builder.set_id(id).build(),
+        )
---
+++ Domains/Sessions/ProductSession.py
@@ -0,0 +1,172 @@
+import __init__
+from dataclasses import dataclass
+from abc import ABCMeta, abstractmethod
+from typing import Optional, Self
+from uuid import uuid4, UUID
+import json
+from pathlib import Path
+
+
+from Commons.helpers import check_hex_string
+from Domains.Sessions import ISessionSerializeable, ISesseionBuilder
+from Domains import ID
+from Domains.Members import *
+from Builders.Members import *
+from Domains.Products import *
+
+from icecream import ic
+
+
+class ProductSession(Product, ISessionSerializeable):
+
+    def serialize_key(self) -> str:
+        return self.id.get_id()
+
+    def serialize_value(self) -> str:
+        return json.dumps(
+            {
+                "seller_id": self.seller_id,
+                "name": self.name,
+                "price": self.price,
+                "description": self.description,
+            }
+        )
+
+
+@dataclass(frozen=True)
+class ProductTempSession:
+    key: UUID
+    product: Product
+    img_id: Optional[Path]
+
+    def get_id(self) -> str:
+        return self.key.hex
+
+    def serialize_key(self) -> str:
+        return self.get_id()
+
+    def serialize_value(self) -> str:
+        return json.dumps(
+            {
+                "seq": str(self.seq),
+                "member_id": self.member_id.get_id(),
+                "name": self.name,
+                "role": str(self.role),
+            },
+            ensure_ascii=False,
+        )
+
+
+class MemberSessionBuilder(ISesseionBuilder):
+    def __init__(
+        self,
+        key: Optional[UUID] = None,
+        member_id: Optional[MemberID] = None,
+        name: Optional[str] = None,
+        seq: int = -1,
+    ):
+        self.seq = seq
+        self.key = key
+        self.mid = member_id
+        self.name: Optional[str] = name
+        self.role: Optional[RoleType] = None
+
+    def set_deserialize_key(self, key: str) -> Self:
+        self.set_key(key)
+        return self
+
+    def set_deserialize_value(self, value: str) -> Self:
+        assert isinstance(value, str), "Type of value is str."
+        try:
+            to_dict = json.loads(value)
+        except:
+            assert False, "The value is not converted to JSON."
+        assert isinstance(to_dict, dict), "Type of convert value is Dict."
+
+        dict_key = "member_id"
+        assert isinstance(to_dict.get(dict_key), str), f"{dict_key} is not exsist dict."
+        self.set_member_id(to_dict.get(dict_key))
+
+        dict_key = "seq"
+        assert isinstance(to_dict.get(dict_key), str), f"{dict_key} is not exsist dict."
+        self.set_seqence(int(to_dict.get(dict_key)))
+
+        dict_key = "name"
+        assert isinstance(to_dict.get(dict_key), str), f"{dict_key} is not exsist dict."
+        self.set_name(to_dict.get(dict_key))
+
+        dict_key = "role"
+        assert isinstance(to_dict.get(dict_key), str), f"{dict_key} is not exsist dict."
+        self.set_role(to_dict.get(dict_key))
+
+        return self
+
+    def set_name(self, name: str) -> Self:
+        assert self.name is None, "name is already set."
+        assert isinstance(name, str), "Type of name is str"
+
+        self.name = name
+        return self
+
+    def set_role(self, role: str) -> Self:
+        assert self.role is None, "rule is already set."
+        assert isinstance(role, str), "Type of rule is str."
+
+        self.role = RoleType[role.strip(" \n\t").upper()]
+        assert self.role.name in list(
+            RoleType._member_names_
+        ), "Type of rule is RuleType. "
+
+        return self
+
+    def set_seqence(self, seq: int) -> Self:
+        assert isinstance(seq, int), "Type of seq is int."
+        assert self.seq < 0, "The sequence is already set."
+        assert seq >= 0, "seq >= 0"
+
+        self.seq = seq
+        return self
+
+    def set_key(self, key: Optional[str] = None) -> Self:
+        assert self.key is None, "The Key is already set."
+        match key:
+            case None:
+                self.key = uuid4()
+            case k if isinstance(key, str):
+                assert check_hex_string(k), "The uuid_hex is not in hex format."
+                self.key = UUID(hex=key)
+            case _:
+                assert False, "Type of key is str."
+
+        return self
+
+    def set_member_id(self, member_id: Optional[str] = None) -> Self:
+        assert self.mid is None, "member id is already set."
+
+        if member_id is None:
+            id = MemberIDBuilder().set_uuid().build()
+        elif isinstance(member_id, str):
+            id = MemberIDBuilder().set_uuid(member_id).build()
+        else:
+            assert False, "Type of member_id is str."
+
+        assert isinstance(
+            id, MemberID
+        ), "ValueType Error: Initialize the id via MemberIDBuilder."
+
+        self.mid = id
+        return self
+
+    def build(self) -> MemberSession:
+        assert isinstance(self.mid, MemberID), "You didn't set the member_id."
+        assert isinstance(self.key, UUID), "You didn't set the key."
+        assert isinstance(self.role, RoleType), "You didn't set the rule."
+        assert isinstance(self.name, str), "You didn't set the name."
+
+        return MemberSession(
+            seq=self.seq,
+            key=self.key,
+            member_id=self.mid,
+            role=self.role,
+            name=self.name,
+        )
