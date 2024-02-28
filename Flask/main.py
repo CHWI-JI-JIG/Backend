62415e1633024be5aeb947ca4b8362ce22f26d5e
ho <lsjkeh9683@naver.com>
2024-02-28 14:11:49 +0900

    0228Ho1

--- Flask/main.py
+++ Flask/main.py
@@ -3,17 +3,21 @@ import __init__
 from flask import Flask, session, jsonify, request
 from Applications.Members.CreateMemberService import CreateMemberService
 from Applications.Members.LoginMemberService import AuthenticationMemberService
+from Applications.Products.ReadProductService import ReadProductService
 from get_config_data import get_db_padding
 from icecream import ic
 
 from Storages.Members.MySqlSaveMember import  MySqlSaveMember
 from Storages.Members.LoginVerifiableAuthentication import LoginVerifiableAuthentication
 from Storages.Sessions.MakeSaveMemberSession import MakeSaveMemberSession
+from Storages.Sessions.MySqlLoadSession import MySqlSaveSession
+from Storages.Products.MySqlGetProduct import MySqlGetProduct
 from result import Result, Ok, Err
 from Domains.Sessions import MemberSession
 from mysql_config import mysql_db
 
 import sys
+import math
 from pathlib import Path
 import json
 from flask_cors import CORS
@@ -44,8 +48,82 @@ def check_id_duplicate(account):
         return True
     else:
         return False    
-        
 
+@app.route('/api/search', methods=['GET'])
+def search():
+    keyword = request.args.get('keyword', type=str)
+    keyword = '%'+keyword+'%'
+    
+    db = pymysql.connect(**mysql_db)
+    
+    page = request.args.get('page', type=int)
+    page -= 1
+    totalCount = 0
+    totalPage = 0
+    size = 20
+    
+    data = []
+    
+    try:
+        with db.cursor() as cursor:
+            
+            sql = f"SELECT COUNT(*) as count FROM {get_db_padding()}product WHERE name LIKE %s"
+            cursor.execute(sql, (keyword,))
+            result = cursor.fetchone()
+            ic(result)
+            if result and isinstance(result[0], int):
+                totalCount = result[0]
+                totalPage = math.ceil(totalCount/size)
+            ic(totalCount)
+            offset = page * size
+            sql = f"SELECT * FROM {get_db_padding()}product WHERE name LIKE %s LIMIT %s, %s"
+            cursor.execute(sql, (keyword,offset, size))
+            data = cursor.fetchall()
+    finally:
+        db.close()
+        
+    response = {
+        'page': page+1,
+        'totalPage' : totalPage,
+        'totalCount' : totalCount,
+        'size' : size,
+        'data' : data
+        
+    }
+    
+    return jsonify(response)
+
+@app.route('/api/products', methods=['get'])
+def product():
+    
+    get_product_repo = MySqlGetProduct(get_db_padding())
+    load_session_repo = MySqlSaveSession(get_db_padding())
+    
+    get_product_info = ReadProductService(get_product_repo, load_session_repo)
+    
+    page = request.args.get('page', type=int)
+    page -= 1
+    size = 3
+    result = get_product_info.get_product_data_for_main_page(page, size)
+    response_data = {"page":page+1, "size": size,"data": []}
+    
+    match result:
+        case Ok((max, products)):
+            
+            response_data["totalPage"] = math.ceil(max/size)
+            for v in products:
+                product_data = {
+                    "productId" : str (v.id.uuid),
+                    "sellerId" : str(v.seller_id.uuid),
+                    "productName" : v.name,
+                    "productImgUrl" : v.img_path,
+                    "productPrice" : v.price
+                }
+                response_data["data"].append(product_data)
+            return jsonify(response_data)
+            
+        case Err(e):
+            return jsonify({'success': False})     
 
 @app.route('/api/login', methods=['POST'])
 def login():
@@ -68,7 +146,7 @@ def login():
             session['auth'] = member_session.role.name
             return jsonify({'success': True, 'certification' : True,'key': member_session.get_id(), 'auth' : str(member_session.role.name), 'name': str(member_session.name)}), 200
         case Err(e):
-            return jsonify({'success': False}), 401
+            return jsonify({'success': False})
 
 @app.route('/api/check-id', methods=['GET'])
 def check_id():
