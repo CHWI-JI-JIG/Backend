69f217ba668743baa6bec0de33bd8697a26c1343
taktakgyun <vbmrkk@naver.com>
2024-02-28 17:27:37 +0900

    🔥 🔥 ⚡️ 🔥 🔥 🔥 🔥 🔥 🔥  : Warming WarmingWarmingWarmingWarmingWarmingWarmingWarmingWarmingWarmingWarmingWarmingWarming

--- Applications/Products/CreateProductService.py
+++ Applications/Products/CreateProductService.py
@@ -20,18 +20,39 @@ class CreateProductService:
         # read_member_repo: IReadableMember,
         save_product: ISaveableProduct,
         save_product_session: ISaveableProductTempSession,
-        load_Product_session: ILoadableSession,
+        load_session: ILoadableSession,
     ):
-        # self.read_repo = read_member_repo
         assert issubclass(
             type(save_product), ISaveableMember
         ), "save_member_repo must be a class that inherits from ISaveableMember."
 
         self.product_repo = save_product
 
+        assert issubclass(
+            type(save_product_session), ISaveableProductTempSession
+        ), "save_member_repo must be a class that inherits from ISaveableProductTempSession."
+
+        self.save_session_repo = save_product_session
+
+        assert issubclass(
+            type(load_session), ILoadableSession
+        ), "save_member_repo must be a class that inherits from ILoadableSession."
+
+        self.load_repo = load_session
+
     def publish_temp_product_id(
         self, member_session_key: str
-    ) -> Result[ProductTempSession, str]: ...
+    ) -> Result[ProductTempSession, str]:
+        # check member session
+        match self.load_repo.load_session(member_session_key).unwrap():
+            case Ok(json):
+                MemberSessionBuilder().set_deserialize_key(
+                    member_session_key
+                ).set_deserialize_value(json).unwrap().build()
+            case _:
+                return Err("plz login")
+
+        # publish
 
     def check_upload_image_path(
         self,
