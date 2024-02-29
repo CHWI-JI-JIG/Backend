7d619ad3bc25fbdc1d1fdf8ffee5df498f2560ce
taktakgyun <vbmrkk@naver.com>
2024-02-29 16:35:06 +0900

    👷 📝 : ci gogogogo

--- init_product_data.py
+++ init_product_data.py
@@ -9,7 +9,7 @@ from Domains.Products import ProductID, Product
 from Domains.Comments import Comment, CommentID
 from datetime import datetime
 from icecream import ic
-import pandas as pd
+
 
 def init_product():
     from Storages.Products import MySqlGetProduct, MySqlSaveProduct
@@ -22,11 +22,11 @@ def init_product():
     id = ProductIDBuilder().set_uuid().set_seqence(1).build()
 
     # CSV 파일 경로
-    csv_path = 'docs/init_product_list.csv'
-    
-    with open(csv_path, 'r', encoding='utf-8') as csvfile:
+    csv_path = "docs/init_product_list.csv"
+
+    with open(csv_path, "r", encoding="utf-8") as csvfile:
         csvreader = csv.DictReader(csvfile)
-        
+
         for row in csvreader:
             id = ProductIDBuilder().set_uuid().set_seqence(1).build()
 
@@ -34,10 +34,10 @@ def init_product():
                 Product(
                     id=id,
                     seller_id=member_list[0],
-                    name=row['상품 이름'],
-                    img_path=row['상품 이미지'],
-                    price=str(row['상품 가격']),
-                    description=row['상품 설명'],
+                    name=row["상품 이름"],
+                    img_path=row["상품 이미지"],
+                    price=str(row["상품 가격"]),
+                    description=row["상품 설명"],
                     register_day=datetime.now(),
                 )
             ):
