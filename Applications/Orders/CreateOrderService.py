6cbd517910af04d5e45c9c7daca70cbe1c9b2bd2
taktakgyun <vbmrkk@naver.com>
2024-02-29 09:46:16 +0900

    ✨ feat : App/Order

---
+++ Applications/Orders/CreateOrderService.py
@@ -0,0 +1,48 @@
+import __init__
+from abc import ABCMeta, abstractmethod
+from typing import Optional
+from result import Result, Ok, Err
+
+from Domains.Members import *
+from Domains.Products import *
+from Domains.Comments import *
+from Domains.Sessions import *
+from Builders.Members import *
+from Repositories.Members import *
+from Repositories.Products import *
+from Repositories.Orders import *
+from Repositories.Products import *
+from Repositories.Sessions import *
+
+from icecream import ic
+
+
+class CreateOrderService:
+    def __init__(
+        self,
+        save_order: ISaveableOrder,
+        load_session: ILoadableSession,
+    ):
+        assert issubclass(
+            type(save_order), ISaveableMember
+        ), "save_member_repo must be a class that inherits from ISaveableMember."
+
+        self.order_repo = save_order
+
+        assert issubclass(
+            type(load_session), ILoadableSession
+        ), "save_member_repo must be a class that inherits from ILoadableSession."
+
+        self.load_repo = load_session
+
+    def create_question(
+        self,
+        product_id: str,
+        user_key: str,
+    ) -> Result[CommentID, str]: ...
+
+    def Add_Answer(
+        self,
+        comment_id: str,
+        user_key: str,
+    ) -> Result[CommentID, str]: ...
