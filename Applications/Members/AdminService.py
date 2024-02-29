5d5a44b11fa0623fd3419882e629c1d3d14eed9e
kjh1232 <kjihee0224@naver.com>
2024-02-29 20:41:52 +0900

    💩 2_29_AdminService_ing

--- Applications/Members/AdminService.py
+++ Applications/Members/AdminService.py
@@ -23,7 +23,7 @@ class AdminService:
         self,
         read_repo: IReadableMemberList,
         edit_repo: IEditableMember,
-        load_session: ILoadableSession,
+        load_session_repo: ILoadableSession,
     ):
         assert issubclass(
             type(read_repo), IReadableMemberList
@@ -32,21 +32,60 @@ class AdminService:
             type(edit_repo), IEditableMember
         ), "save_member_repo must be a class that inherits from  IEditableMember."
         assert issubclass(
-            type(load_session), ILoadableSession
+            type(load_session_repo), ILoadableSession
         ), "save_member_repo must be a class that inherits from ILoadableSession."
 
-        self.read_repo = edit_repo
+        self.read_repo = read_repo
         self.edit_repo = edit_repo
-        self.load_session_repo = load_session
+        self.load_session_repo = load_session_repo
 
     def read_members(
         self,
         page=0,
         size=10,
-    ) -> Result[Tuple[int, List[Member]], str]: ...
+    ) -> Result[Tuple[int, List[Member]], str]:
+        return self.read_repo.get_members(page=page, size=size)
 
     def change_role(
         self,
         role: str,
         user_id: str,
-    ) -> Result[MemberID, str]: ...
+    ) -> Result[MemberID, str]:
+        #load_result = self.read_repo.get_members(user_id)
+        #ic()
+        #match load_result:
+        #    case Ok(member):
+        #        new_role = "seller" if role == "buyer" else "buyer"
+        #        role = new_role
+        #        update_result = self.edit_repo.update_member(member)
+        #        ic( )
+        #        match update_result:
+        #            case Ok(member_id):
+        #                ic()
+        #                Ok(member_id)
+        #            case Err(err_msg):
+        #                ic()
+        #                Err(err_msg)
+        #    case Err(err_msg):
+        #        ic()
+        #        Err(err_msg)
+                
+                
+        load_result = self.read_repo.get_members(user_id)
+        ic()
+        match load_result:
+            case Ok(member):
+                new_role = "seller" if role == "buyer" else "buyer"
+                role = new_role
+                update_result = self.edit_repo.update_member(member)
+                ic( )
+                match update_result:
+                    case Ok(member_id):
+                        ic()
+                        Ok(member_id)
+                    case Err(err_msg):
+                        ic()
+                        Err(err_msg)
+            case Err(err_msg):
+                ic()
+                Err(err_msg)
