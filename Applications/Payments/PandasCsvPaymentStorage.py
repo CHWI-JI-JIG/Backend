f82436e8f7b16ad2ffda4b566374a3e43abcaa92
taktakgyun <vbmrkk@naver.com>
2024-03-02 19:44:21 +0900

    ✨ ➕ 📝 dependency pandas, Add Payment

---
+++ Applications/Payments/PandasCsvPaymentStorage.py
@@ -0,0 +1,107 @@
+import __init__
+from abc import ABCMeta, abstractmethod
+from typing import Optional, Tuple, List
+from result import Result, Ok, Err
+from uuid import UUID
+
+from dataclasses import asdict
+from datetime import datetime
+from icecream import ic
+import pandas as pd
+from csv import DictWriter
+import pymysql
+
+from Applications.Payments import PayData
+from Applications.Payments import IPaymentRepo
+
+
+class PandasCsvPaymentStorage(IPaymentRepo):
+    dict_header = [
+        "id",
+        "seller_bank_account",
+        "seller_id",
+        "seller_name",
+        "buyer_card_account",
+        "buyer_id",
+        "buyer_name",
+        "transfer_time",
+        "withdrawal",
+        "deposit",
+    ]
+
+    def __init__(self, name_padding: str = "log_"):
+        self.name_padding = name_padding
+        self.timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+        # 데이터 로드
+        file_path = self.get_padding_name(f"{self.timestamp}_payment_data.csv")
+        self.file_path = file_path
+        try:
+            self.df: pd.DataFrame = pd.read_csv(file_path)
+        except FileNotFoundError:
+            self.df: pd.DataFrame = pd.DataFrame(columns=self.dict_header)
+        except:
+            assert False, "load error"
+
+    def connect(self):
+        from get_config_data import get_mysql_dict
+
+        sql_config = get_mysql_dict()
+        return pymysql.connect(
+            host=sql_config["host"],
+            user=sql_config["user"],
+            password=sql_config["password"],
+            db=sql_config["database"],
+            charset=sql_config["charset"],
+        )
+
+    def get_padding_name(self, name: str) -> str:
+        return f"{self.name_padding}{name}"
+
+    def save_pay_data(self, data: PayData) -> Result[PayData, str]:
+        # 기존 데이터 로드
+        file_path = self.file_path
+
+        # data를 dict으로 변경
+        new_row = asdict(data)
+        # self.df에 새로운 데이터 추가
+        self.df.loc[len(self.df)] = new_row
+
+        # CSV 파일로 저장
+        self.df.to_csv(file_path, index=False)
+        with open(file_path, "a", newline="") as f_object:
+            writer_obj = DictWriter(f_object, fieldnames=self.dict_header)
+            writer_obj.writerow(new_row)
+
+        return Ok(data)
+
+    def load_pay_data(self, id: UUID) -> Optional[PayData]:
+        # ID로 데이터 검색
+        data_row = self.df.loc[self.df["id"] == id.hex].to_dict(orient="records")
+
+        if not data_row:
+            return None
+
+        return PayData(**data_row[0])
+
+    def load_list_of_pay_data(
+        self,
+        seller_name: Optional[str] = None,
+        buyer_name: Optional[str] = None,
+        page=0,
+        size=10,
+    ) -> Result[Tuple[int, List[PayData]], str]:
+        # 필터링
+        if seller_name:
+            self.df = self.df.loc[self.df["seller_name"] == seller_name]
+        if buyer_name:
+            self.df = self.df.loc[self.df["buyer_name"] == buyer_name]
+
+        total_rows = len(self.df)
+        start_idx = page * size
+        end_idx = start_idx + size
+
+        # 페이지에 맞게 잘라내기
+        page_data = self.df.iloc[start_idx:end_idx].to_dict(orient="records")
+
+        return Ok((total_rows, [PayData(**row) for row in page_data]))
--- Tests/Orders/test_order_payment.py
+++ Tests/Orders/test_order_payment.py
@@ -20,6 +20,7 @@ from Applications.Members.ExtentionMethod import hashing_passwd
 from Applications.Members import *
 from Applications.Products import *
 from Applications.Orders import *
+from Applications.Payments import PayData, PaymentService, PandasCsvPaymentStorage
 
 from Storages.Members import *
 from Storages.Products import *
@@ -54,6 +55,9 @@ class test_order_builder(unittest.TestCase):
         cls.order_migrate = mo
         ms = MySqlCreateSession(get_db_padding())
         cls.session_migrate = ms
+
+        cls.pay_service = PaymentService()
+        cls.csv_path = cls.pay_service.pay_repo.file_path
 
         if ms.check_exist_session():
             ms.delete_session()
@@ -147,7 +151,158 @@ class test_order_builder(unittest.TestCase):
         if self.order_migrate.check_exist_order():
             self.order_migrate.delete_order()
 
+    def test_pay_service(self):
+        "Hook method for deconstructing the test fixture after testing it."
+        print("\t\t", sys._getframe(0).f_code.co_name)
+
+        match self.pay_service.approval_and_logging(
+            transition_key="aaa",
+            total_price=10000,
+            card_account="1111-1111-1111-1111",
+        ):
+            case Ok(success):
+                assert success, "Error"
+            case e:
+                assert False, f"{e}"
+
+        match self.pay_service.approval_and_logging(
+            transition_key="aaa",
+            total_price=100000000,
+            card_account="1111-1111-1111-1111",
+        ):
+            case Err("Insufficient funds in the card"):
+                pass
+            case e:
+                assert False, f"{e}"
+
+        match self.pay_service.approval_and_logging(
+            transition_key="aaa",
+            total_price=300000,
+            card_account="2222-2222-2222-2222",
+        ):
+            case Ok(success):
+                assert success, "Error"
+            case e:
+                assert False, f"{e}"
+
+        match self.pay_service.approval_and_logging(
+            transition_key="aaa",
+            total_price=300000,
+            card_account="2223-2222-2222-2222",
+        ):
+            case Err("Invalid card number"):
+                pass
+            case e:
+                assert False, f"{e}"
+
     def test_회원생성_주문조회_상품조회및선택_트렌젝션생성_결제완료_주문조회(self):
+        "Hook method for deconstructing the test fixture after testing it."
+        print("\t\t", sys._getframe(0).f_code.co_name)
+        # 회원 생성
+        match self.member_create_service.create(
+            account="qawsars",
+            passwd="123",
+            role="buyer",
+            name="lee Tak",
+            phone="01023459087",
+            email="arst@daum.com",
+            address="guri",
+        ):
+            case Ok(member_id):
+                mid = member_id
+            case _:
+                assert False, "Fail Create Member"
+        # login
+        match self.login_service.login("qawsars", "134"):
+            case Ok(session):
+                member_session = session
+                assert False, "Why Login Success?"
+            case Err(e):
+                self.assertEqual(e, "비밀번호가 틀렸습니다.")
+            case e:
+                assert False, f"DB Error? : {e}"
+
+        match self.login_service.login("qawsars", "123"):
+            case Ok(session):
+                member_session = session
+            case e:
+                assert False, f"{e}"
+        # 주문조회
+        match self.read_order_service.get_order_data_for_buyer_page(
+            member_session.get_id()
+        ):
+            case Ok((0, none)):
+                self.assertEqual(len(none), 0)
+            case e:
+                assert False, f"{e}"
+
+        # 상품 조회 및 선택
+        match self.read_product_service.get_product_data_for_main_page(page=0, size=3):
+            case Ok((_, products)):
+                assert len(products) == 3, "Why Not Products num 3"
+                target_product = products[0]
+                self.assertIsInstance(target_product, Product)
+            case e:
+                assert False, f"{e}"
+
+        # 상품 트랜젝션 생성
+        match self.order_and_payment_service.publish_order_transition(
+            recipient_name="Lee Takgyun",
+            recipient_phone="01033452234",
+            recipient_address="guri",
+            product_id=target_product.id.get_id(),
+            buy_count=3,
+            single_price=target_product.price,
+            user_session_key=member_session.get_id(),
+        ):
+            case Ok(trans):
+                order_transition = trans
+            case e:
+                assert False, f"{e}"
+
+        # 결제 완료
+        match self.pay_service.approval_and_logging(
+            order_transition,
+            order_transition.order.total_price,
+            "1111-1111-1111-1111",
+        ):
+            case Ok(True):
+                pass
+            case e:
+                assert False, f"{e}"
+
+        match self.order_and_payment_service.payment_and_approval_order(
+            order_transition_session=order_transition.get_id(),
+            payment_success=True,
+        ):
+            case Ok(id):
+                order_id = id
+            case e:
+                assert False, f"{e}"
+        # 주문조회
+        match self.read_order_service.get_order_data_for_buyer_page(
+            member_session.get_id()
+        ):
+            case Ok((1, orders)):
+                self.assertEqual(len(orders), 1)
+
+                target_order = orders[0]
+
+                self.assertIsInstance(target_order, Order)
+            case e:
+                assert False, f"{e}"
+
+        self.assertEqual(target_order.id.get_id(), order_id.get_id())
+        self.assertEqual(target_order.recipient_name, "Lee Takgyun")
+        self.assertEqual(target_order.recipient_address, "guri")
+        self.assertEqual(target_order.recipient_phone, "01033452234")
+
+        self.assertEqual(target_order.total_price, target_product.price * 3)
+        self.assertEqual(target_order.buy_count, 3)
+        self.assertEqual(target_order.product_name, target_product.name)
+        self.assertEqual(target_order.product_id.get_id(), target_product.id.get_id())
+
+    def test_회원생성_주문조회_상품조회및선택_트렌젝션생성_Fail(self):
         "Hook method for deconstructing the test fixture after testing it."
         print("\t\t", sys._getframe(0).f_code.co_name)
         # 회원 생성
@@ -203,7 +358,7 @@ class test_order_builder(unittest.TestCase):
             recipient_phone="01033452234",
             recipient_address="guri",
             product_id=target_product.id.get_id(),
-            buy_count=3,
+            buy_count=1000,
             single_price=target_product.price,
             user_session_key=member_session.get_id(),
         ):
@@ -213,37 +368,32 @@ class test_order_builder(unittest.TestCase):
                 assert False, f"{e}"
 
         # 결제 완료
+        match self.pay_service.approval_and_logging(
+            order_transition,
+            order_transition.order.total_price,
+            "1111-1111-1111-1111",
+        ):
+            case Err("Insufficient funds in the card"):
+                pass
+            case e:
+                assert False, f"{e}"
+
         match self.order_and_payment_service.payment_and_approval_order(
             order_transition_session=order_transition.get_id(),
-            payment_success=True,
+            payment_success=False,
         ):
-            case Ok(id):
-                order_id = id
+            case Err("Fail Payment"):
+                pass
             case e:
                 assert False, f"{e}"
         # 주문조회
         match self.read_order_service.get_order_data_for_buyer_page(
             member_session.get_id()
         ):
-            case Ok((1, orders)):
-                self.assertEqual(len(orders), 1)
-
-                target_order = orders[0]
-                ic(target_order)
-
-                self.assertIsInstance(target_order, Order)
+            case Ok((0, orders)):
+                self.assertEqual(len(orders), 0)
             case e:
                 assert False, f"{e}"
-
-        self.assertEqual(target_order.id.get_id(), order_id.get_id())
-        self.assertEqual(target_order.recipient_name, "Lee Takgyun")
-        self.assertEqual(target_order.recipient_address, "guri")
-        self.assertEqual(target_order.recipient_phone, "01033452234")
-
-        self.assertEqual(target_order.total_price, target_product.price * 3)
-        self.assertEqual(target_order.buy_count, 3)
-        self.assertEqual(target_order.product_name, target_product.name)
-        self.assertEqual(target_order.product_id.get_id(), target_product.id.get_id())
 
 
 def main():
