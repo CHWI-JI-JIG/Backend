072749aeea3c5c5d471ea050b6b326cf723031e6
taktakgyun <vbmrkk@naver.com>
2024-03-19 21:46:20 +0900

    ⚡️

--- Domains/ID.py
+++ Domains/ID.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
 from abc import ABCMeta, abstractmethod
 from typing import Optional, Self
-from uuid import uuid4, UUID
 
 
 class ID(metaclass=ABCMeta):
     @abstractmethod
     def get_id(self) -> str: ...
+    
+    # @abstractmethod
+    # def get_seq(self) -> Optional[int]: ...
