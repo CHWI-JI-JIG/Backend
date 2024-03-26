939af27f538f6f0da5a9a7b14323b3e666187437
js-u-u <vjsdy5@gmail.com>
2024-03-26 17:47:57 +0900

    🐛 main-login

--- Flask/main.py
+++ Flask/main.py
@@ -326,6 +326,7 @@ def login():
                         "name": member_session.name,
                     }
                 ),
+                200
             )
         case Err(e):
             return jsonify({"success": False})
@@ -348,7 +349,7 @@ def changeExpiredPw():
 
     match result:
         case Ok(_):            
-            return jsonify({"success": True}    200,
+            return jsonify({"success": True}, 200
             )
         case Err(e):
             return jsonify({"success": False})
